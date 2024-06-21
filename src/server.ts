922cffdffaacd89035eebc8290703e6c98bdeeac
lukaafff <luizagarcia2405@gmail.com>
2024-06-21 20:17:14 -0300

    instalando cors, prisma, express async errors

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,23 @@
 import express, {Request, Response, NextFunction} from "express"
+import "express-async-errors";
 import {router} from './routes';
 
 const app = express();
 const port = 3000;
 app.use(express.json());
 app.use(router);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    if(err instanceof Error) {
+        return res.status(400).json({
+            error: err.message,
+        });
+    }
+    return res.status(500).json({
+        status: 'error',
+        message: "Internal Server Error",
+    });
+});
 
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`);
