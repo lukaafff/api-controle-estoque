5a4b36b74b62af95954978df7c697b2c6cf64ec9
lukaafff <luizagarcia2405@gmail.com>
2024-06-22 00:20:58 -0300

    deletar usuario params

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { Router, Request, Response } from "express";
 import {CreateUserController} from "./controllers/user/CreateUserController";
 import {AuthUserController} from "./controllers/user/AuthUserController";
 import {DetailUserController} from "./controllers/user/DetailUserController";
+import {RemoveUserController} from "./controllers/user/RemoveUserController";
 import {isAuthenticated} from "./middlewares/isAuthenticated";
 
 const router = Router();
@@ -14,5 +15,6 @@ router.get("/test", (req: Request, res: Response) => {
 router.post('/user', new CreateUserController().handle);
 router.post('/session', new AuthUserController().handle);
 router.get('/me', isAuthenticated, new DetailUserController().handle);
+router.delete('/user/remove', new RemoveUserController().handle);
 
 export {router};
