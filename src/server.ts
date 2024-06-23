155c0ad26386225259819f4e260af881567b975b
lukaafff <luizagarcia2405@gmail.com>
2024-06-22 22:21:39 -0300

    configurando swagger

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,18 @@
 import express, {Request, Response, NextFunction} from "express"
 import "express-async-errors";
 import {router} from './routes';
+import cors from "cors";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "../swagger.json";
+import path from "path";
 
 const app = express();
 const port = 3000;
 app.use(express.json());
-app.use(router);
+app.use(cors());
+app.use("v1" ,router);
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/files', express.static(path.resolve(__dirname, "..", "tmp")));
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     if(err instanceof Error) {
@@ -17,6 +24,12 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
         status: 'error',
         message: "Internal Server Error",
     });
+});
+
+app.get('/terms', (req: Request, res: Response) => {
+    return res.json({
+        message: "Termos de serviço"
+    })
 });
 
 app.listen(port, () => {
